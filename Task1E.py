c42ea41a37d5587626219d18856031272d69f1f5
Abbie Wallace <acw81@cam.ac.uk>
2022-02-10 13:48:47 +0000

    update

--- Task1E.py
+++ Task1E.py
@@ -2,9 +2,18 @@ from floodsystem.stationdata import build_station_list
 from floodsystem.geo import rivers_with_station
 from floodsystem.geo import stations_by_river
 from floodsystem.geo import rivers_by_station_number
+from floodsystem.geo import no_of_stations
 
 stations = build_station_list()
 rivers = rivers_with_station(stations)
+rivers_list=list(rivers)
+
+rivers_list.sort()
+
+print(len(rivers_list))
+print(rivers_list[:10])
+
+stationsByRiver = stations_by_river(stations)
 
 station_number=rivers_by_station_number(rivers, 9)
 print(station_number)
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -34,29 +34,26 @@ def rivers_with_station(stations):
     return set_river
 
 def stations_by_river(stations):
-    station_river = {}
+    station_river = dict
     for i in rivers_with_station(stations):
         stationsAtRiver = []
         for station in stations:
-            if i == station.river:
+            if i == station_river:
                 stationsAtRiver.append(station.name)
         station_river.update({i: stationsAtRiver})
     return station_river
 
-def no_of_stations(station_river):
-    station_no_list= {}
-    for key, value in station_river():
-        j=0
-        stations=value
-        for i in stations:
-            j+=1
-            station_no_list.update({key:j})
+def no_of_stations(station):
+    Station=stations_by_river(station)
+    station_no_list=[]
+    for i in Station:
+        station_no_list.append((i, len(Station[i])))
     return station_no_list
 
 def rivers_by_station_number(stations, N):
     stations_numbers=no_of_stations(stations)
     sorted_stations=sorted(stations_numbers.items(), key=lambda y: y[1], reverse=True)
-    output_stations={}
+    output_stations=dict
     i=0
     k=0
     for key, value in sorted_stations:
