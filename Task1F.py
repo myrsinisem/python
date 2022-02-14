58b736e8ff698b90879e1f8a43b96244e18106d6
acw8111 <98668934+acw8111@users.noreply.github.com>
2022-02-14 11:20:21 +0000

    Add files via upload

--- Task1F.py
+++ Task1F.py
@@ -2,6 +2,7 @@ from floodsystem.stationdata import build_station_list
 from floodsystem.station import inconsistent_typical_range_stations
 
 
-stations=build_station_list
+stations=build_station_list()
 inconsistent_stations=inconsistent_typical_range_stations(stations)
-print(inconsistent_stations.sort())
+sorted_stations=sorted(inconsistent_stations)
+print(sorted_stations)
