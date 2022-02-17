aab24ee78806405d889a2c2fab891a961fcaf03d
myrsinisem <92521891+myrsinisem@users.noreply.github.com>
2022-02-17 10:57:22 +0000

    Add files via upload

---
+++ test_Task1A.py
@@ -0,0 +1,25 @@
+from floodsystem.geo import stations_by_distance
+from floodsystem.stationdata import build_station_list
+
+
+def run():
+    """Requirements for Task 1A"""
+
+    # Build list of stations
+    stations = build_station_list()
+    assert(len(stations))== 2165
+
+    # Print number of stations
+    print("Number of stations: {}".format(len(stations)))
+
+    # Display data from 3 stations:
+    for station in stations:
+        if station.name in [
+                'Bourton Dickler', 'Surfleet Sluice', 'Gaw Bridge'
+        ]:
+            print(station)
+
+
+if __name__ == "__main__":
+    print("*** Task 1A: CUED Part IA Flood Warning System ***")
+    run()
