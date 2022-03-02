af80bac9eefc3cafd46d693169bcb3258cf6a37b
acw8111 <98668934+acw8111@users.noreply.github.com>
2022-03-02 13:32:39 +0000

    Add files via upload

---
+++ Task2F.py
@@ -0,0 +1,21 @@
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.stationdata import build_station_list , update_water_levels
+from floodsystem.datafetcher import fetch_latest_water_level_data, fetch_station_data
+import datetime
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.stationdata import build_station_list
+from floodsystem.plot import plot_water_level_with_fit
+
+stations = build_station_list()
+N=10
+
+update_water_levels(stations)
+stations_high_threat = stations_highest_rel_level(stations, N)
+j=0
+for i in range (0,4):
+    station=stations_high_threat[i]
+    print(station)
+    dt = 2
+    dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+    plot_water_level_with_fit(station, dates, levels, 4)
+    j+=1
