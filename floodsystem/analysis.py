51476b00f2c59e87aa7b539e98769f21e69ee2c7
acw8111 <98668934+acw8111@users.noreply.github.com>
2022-03-02 13:31:43 +0000

    Add files via upload

---
+++ floodsystem/analysis.py
@@ -0,0 +1,24 @@
+import matplotlib as plt
+import numpy as np
+
+def polyfit(dates, levels, p):
+    dates_as_num=plt.dates.date2num(dates)
+    a=dates_as_num[0]#
+    dates2=()
+    for i in dates_as_num:
+        j=i-a
+        dates2.append(j)
+        
+    # Create set of 10 data points on interval (0, 2)
+    
+    # Find coefficients of best-fit polynomial f(x) of degree 4
+    p_coeff = np.polyfit(dates2, levels, p)
+    print (p_coeff)
+
+    # Convert coefficient into a polynomial that can be evaluated,
+    # e.g. poly(0.3)
+    poly = np.poly1d(p_coeff)
+    print(poly)
+
+    return poly, a
+
