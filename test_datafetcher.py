4ffd3c3c455f7b65d417de30f28011da23019a13
myrsinisem <92521891+myrsinisem@users.noreply.github.com>
2022-03-02 11:47:02 +0000

    datafetcher and plot tests

---
+++ test_datafetcher.py
@@ -0,0 +1,69 @@
+# Copyright (C) 2018 Garth N. Wells
+#
+# SPDX-License-Identifier: MIT
+"""Unit test for the stationdata module"""
+
+import datetime
+from xml.dom.xmlbuilder import _DOMInputSourceCharacterStreamType
+
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.stationdata import build_station_list
+from floodsystem.datafetcher import fetch
+import dateutil.parser
+
+
+def test_build_station_list():
+
+    # Build list of stations
+    stations = build_station_list()
+
+    # Find station 'Cam'
+    for station in stations:
+        if station.name == 'Cam':
+            station_cam = station
+            break
+
+    # Assert that station is found
+    assert station_cam
+
+    # Fetch data over past 2 days
+    dt = 2
+    dates2, levels2 = fetch_measure_levels(
+        station_cam.measure_id, dt=datetime.timedelta(days=dt))
+    assert len(dates2) == len(levels2)
+
+    # Fetch data over past 10 days
+    dt = 10
+    dates10, levels10 = fetch_measure_levels(
+        station_cam.measure_id, dt=datetime.timedelta(days=dt))
+    assert len(dates10) == len(levels10)
+    assert len(dates10) > len(levels2)
+
+def test_fetch_measure_levels(measure_id,dt):
+    now = datetime.datetime.utcnow()
+    start = now - dt
+    dt =0
+    assert now == start 
+
+    url_base = measure_id
+    url_options = "/readings/?_sorted&since=" + start.isoformat() + 'Z'
+    url = url_base + url_options
+    data = fetch(url)
+
+    dates,levels = []
+    for measure in data['items']:
+    
+        d = dateutil.parser.parse(measure['dateTime'])
+
+        # Append data
+        dates.append(d)
+        levels.append(measure['value'])
+
+        assert len(dates) > 0
+        assert len(levels) > 0
+
+
+
+
+
+
