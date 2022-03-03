7d207d990c63f08fe49f1047f0e0ada4aed90d6e
myrsinisem <92521891+myrsinisem@users.noreply.github.com>
2022-03-03 10:26:33 +0000

    update tests for flood and plot

---
+++ test_flood.py
@@ -0,0 +1,39 @@
+from ast import Continue
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.station import MonitoringStation, relative_water_level
+from floodsystem.flood import stations_level_over_threshold, stations_highest_rel_level
+from floodsystem.datafetcher import fetch_latest_water_level_data, fetch_station_data
+     
+stations = build_station_list()
+
+update_water_levels(stations)
+
+def test_stations_level_over_threshold1():
+    tol = 0.2
+    stations_thresh = stations_level_over_threshold(stations, tol)
+    list = []
+    for station in stations_thresh:
+        list.append(station[0].name) 
+    assert len(list) > 10
+
+#tolerance is low 0.2 so it should always print more than 10 --> test is universal
+
+
+def test_stations_level_over_threshhold2():
+    tol = 0.8
+    update_water_levels(stations)
+    stations_above_level = stations_level_over_threshold(stations, tol)
+
+    for stations in stations_above_level:
+        assert stations[1] > 0.8
+        assert stations[2] > 0.8
+
+
+
+stations_high_threat = stations_highest_rel_level(stations, 10)
+     
+list2 = []
+for station in stations_high_threat:
+    list2.append(station[0].name)
+
+assert len(list2) == 10
---
+++ test_plot.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+from floodsystem.plot import plot_water_levels, plot_water_level_with_fit
+from floodsystem.station import MonitoringStation
+import matplotlib.dates as date
+
+def test_plot_water_levels():
+    
+    #creating internal data
+    s_id = "test-s-id"
+    m_id = "test-m-id"
+    label = "some station"
+    coord = (1.0, 1.0)
+    trange = (0.5, 1.0)
+    river = "River"
+    town = "Town"
+    s = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
+    s.set_latestLevel(1)
+    y = [0,1,2,3,4,5]
+    x = y
+    plot_water_levels(s,x,y)
+    np.testing.assert_array_equal(y,x)
+
+
+def test_plot_level_with_fit():
+    s_id = "test-s-id"
+    m_id = "test-m-id"
+    label = "some station"
+    coord = (-2.0, 4.0)
+    trange = (0.5, 1.5)
+    river = "River X"
+    town = "My Town"
+    s = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
+    s.set_latestLevel(1)
+    levels = [216, 125, 64, 27, 16, 1]
+    dates = date.num2date([6, 5, 4, 3, 2, 1])
+    p=3
+    plot_water_level_with_fit(s, dates, levels, p)
