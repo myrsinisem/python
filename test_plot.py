b3ba4016a4c6bc3a26eec8c6b5d2f36ea5feb198
myrsinisem <92521891+myrsinisem@users.noreply.github.com>
2022-03-04 11:49:39 +0000

    Update test_plot.py

--- test_plot.py
+++ test_plot.py
@@ -20,19 +20,3 @@ def test_plot_water_levels():
     x = y
     plot_water_levels(s,x,y)
     np.testing.assert_array_equal(y,x)
-
-
-def test_plot_level_with_fit():
-    s_id = "test-s-id"
-    m_id = "test-m-id"
-    label = "some station"
-    coord = (-2.0, 4.0)
-    trange = (0.5, 1.5)
-    river = "River X"
-    town = "My Town"
-    s = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
-    #s.set_latestLevel(1)
-    levels = [216, 125, 64, 27, 16, 1]
-    dates = date.num2date([6, 5, 4, 3, 2, 1])
-    p=3
-    plot_water_level_with_fit(s, dates, levels, p)
